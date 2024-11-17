e12a5a8d891270f1e914e9519068b2dd3d47a0d2
teeliaa <110159064+teeliaa@users.noreply.github.com>
2024-11-17 16:09:14 +0900

    Update slide.js

--- slide.js
+++ slide.js
@@ -26,3 +26,32 @@
       console.error("iframe 요소를 찾을 수 없습니다.");
     }
 };
+
+window.showFullSectionIframe = function(src) {
+    // 'left'와 'right' 영역 숨기기
+    document.querySelector('.left').style.display = 'none';
+    document.querySelector('.right').style.display = 'none';
+
+    // section 영역 비우기
+    const section = document.querySelector('section');
+    section.innerHTML = ''; // 기존 내용 지우기
+
+    // 새 iframe 요소 생성
+    const iframe = document.createElement('iframe');
+    iframe.src = src;
+    iframe.frameborder = '0';
+    iframe.style.width = '70%';
+    iframe.style.height = '100%';
+    iframe.style.border = 'none';
+
+    if (src.includes('gage_cal.html')) {
+       // gage_cal.html일 때, 전체 페이지의 높이를 조정
+       document.documentElement.style.height = '100%'; // HTML의 높이를 100%로 설정
+       document.body.style.height = '100%'; // BODY의 높이를 100%로 설정
+       document.documentElement.style.overflow = 'hidden'; // 바깥 스크롤 숨기기
+       section.style.height = '100vh'; // section의 높이를 화면 크기와 맞춤
+   }
+
+    // section에 iframe 추가
+    section.appendChild(iframe);
+  }
